60c8216edeafec05a53256627ba57b7d20f39857
gumsu <chavi9@naver.com>
2021-01-30 22:53:30 +0900

    [#1] 라디오 버튼 클릭 시 정보 가져오기

--- app/src/main/java/com/gdh/retrofit_practice/MainActivity.kt
+++ app/src/main/java/com/gdh/retrofit_practice/MainActivity.kt
@@ -2,10 +2,40 @@ package com.gdh.retrofit_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.gdh.retrofit_practice.utils.Constants.TAG
+import com.gdh.retrofit_practice.utils.SEARCH_TYPE
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var currentSearchType : SEARCH_TYPE = SEARCH_TYPE.PHOTO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "MainActivity - onCreate() called")
+
+        // 라디오 그룹 가져오기
+        search_term_radio_group.setOnCheckedChangeListener { _, checkedId ->
+
+            // switch 문
+            when(checkedId){
+                R.id.photo_search_radio_btn -> {
+                    Log.d(TAG, "사진검색 버튼 클릭")
+                    search_term_text_layout.hint = "사진검색"
+                    search_term_text_layout.startIconDrawable = resources.getDrawable(R.drawable.ic_baseline_photo_library_24, resources.newTheme())
+                    this.currentSearchType = SEARCH_TYPE.PHOTO
+                }
+                R.id.user_search_radio_btn -> {
+                    Log.d(TAG, "사용자검색 버튼 클릭")
+                    search_term_text_layout.hint = "사용자검색"
+                    search_term_text_layout.startIconDrawable = resources.getDrawable(R.drawable.ic_baseline_person_24, resources.newTheme())
+                    this.currentSearchType = SEARCH_TYPE.USER
+                }
+            }
+            Log.d(TAG, "MainActivity - onCheckedChanged() called // currentSearchType: $currentSearchType")
+        }
     }
 }
