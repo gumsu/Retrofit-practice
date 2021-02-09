2e0bffc78f7593f9e16f7f577e6ae1f1c1d74c1d
gumsu <chavi9@naver.com>
2021-02-09 23:48:45 +0900

    [#3] 로깅 인터셉터를 okhttp 클라이언트에 추가 - 에러 확인

--- app/src/main/java/com/gdh/retrofit_practice/retrofit/RetrofitClient.kt
+++ app/src/main/java/com/gdh/retrofit_practice/retrofit/RetrofitClient.kt
@@ -24,7 +24,7 @@ object RetrofitClient {
         // okhhtp 인스턴스 생성
         val client = OkHttpClient.Builder()
 
-        // 로그를 찍기 위해 로깅 인터셉터 추가
+        // 로그를 찍기 위해 로깅 인터셉터 설정
        val loggingInterceptor = HttpLoggingInterceptor(object : HttpLoggingInterceptor.Logger {
            override fun log(message: String) {
                Log.d(TAG, "RetrofitClient - log() called / message: $message ")
@@ -44,13 +44,21 @@ object RetrofitClient {
                }
            }
        })
-        
+
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        // 위에서 설정한 로깅 인터셉터를 okhttp 클라이언트에 추가
+        client.addInterceptor(loggingInterceptor)
+
         if(retrofitClient == null){
 
             // 레트로핏 빌더를 통해 인스턴스 생성
             retrofitClient = Retrofit.Builder()
                 .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
+
+                    // 위에서 설정한 클라이언트로 레트로핏 클라이언트를 설정한다.
+                .client(client.build())
                 .build()
         }
         return retrofitClient
