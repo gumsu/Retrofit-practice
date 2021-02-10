134807ba309b4b4bb7a78b7c19b69732d8bfc8ff
gumsu <chavi9@naver.com>
2021-02-10 23:39:10 +0900

    [#3] intent로 받아온 데이터 저장, 로그로 확인

--- app/src/main/java/com/gdh/retrofit_practice/PhotoCollectionActivity.kt
+++ app/src/main/java/com/gdh/retrofit_practice/PhotoCollectionActivity.kt
@@ -3,13 +3,23 @@ package com.gdh.retrofit_practice
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.gdh.retrofit_practice.model.Photo
 import com.gdh.retrofit_practice.utils.Constants.TAG
 
 class PhotoCollectionActivity: AppCompatActivity() {
+
+    var photoList = ArrayList<Photo>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_collection)
 
         Log.d(TAG, "PhotoCollectionActivity - onCreate() called ")
+
+        val bundle = intent.getBundleExtra("array_bundle")
+        val searchTerm = intent.getStringExtra("search_term")
+        photoList = bundle?.getSerializable("photo_array_list") as ArrayList<Photo>
+
+        Log.d(TAG, "PhotoCollectionActivity - onCreat() called / searchTerm : $searchTerm, photoList.count() : ${photoList.count()} ")
     }
 }
