443cb5c3ee8db2be3421cf10bd245ee78c6035e2
gumsu <chavi9@naver.com>
2021-02-11 00:19:51 +0900

    [#3] photo item 레이아웃 작성 / recyclerview 아답터, 뷰홀더 생성

---
+++ app/src/main/java/com/gdh/retrofit_practice/recyclerview/PhotoGridRecyclerViewAdapter.kt
@@ -0,0 +1,6 @@
+package com.gdh.retrofit_practice.recyclerview
+
+import androidx.recyclerview.widget.RecyclerView
+
+class PhotoGridRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+}
---
+++ app/src/main/java/com/gdh/retrofit_practice/recyclerview/PhotoItemViewHolder.kt
@@ -0,0 +1,7 @@
+package com.gdh.retrofit_practice.recyclerview
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class PhotoItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+}
