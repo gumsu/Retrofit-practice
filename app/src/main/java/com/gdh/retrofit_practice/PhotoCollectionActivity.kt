fb3223b4b246247251de1445e64a019a014dbfb0
gumsu <chavi9@naver.com>
2021-02-11 00:38:45 +0900

    [#3] mainActivity에서 어댑터 연결

--- app/src/main/java/com/gdh/retrofit_practice/PhotoCollectionActivity.kt
+++ app/src/main/java/com/gdh/retrofit_practice/PhotoCollectionActivity.kt
@@ -3,12 +3,19 @@ package com.gdh.retrofit_practice
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import com.gdh.retrofit_practice.model.Photo
+import com.gdh.retrofit_practice.recyclerview.PhotoGridRecyclerViewAdapter
 import com.gdh.retrofit_practice.utils.Constants.TAG
+import kotlinx.android.synthetic.main.activity_photo_collection.*
 
 class PhotoCollectionActivity: AppCompatActivity() {
 
+    // 데이터
     var photoList = ArrayList<Photo>()
+
+    // 어댑터
+    private lateinit var photoGridRecyclerViewAdapter: PhotoGridRecyclerViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +28,11 @@ class PhotoCollectionActivity: AppCompatActivity() {
         photoList = bundle?.getSerializable("photo_array_list") as ArrayList<Photo>
 
         Log.d(TAG, "PhotoCollectionActivity - onCreat() called / searchTerm : $searchTerm, photoList.count() : ${photoList.count()} ")
+
+        this.photoGridRecyclerViewAdapter = PhotoGridRecyclerViewAdapter()
+        this.photoGridRecyclerViewAdapter.submitList(photoList)
+
+        my_photo_recycler_view.layoutManager = GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false)
+        my_photo_recycler_view.adapter = this.photoGridRecyclerViewAdapter
     }
 }
