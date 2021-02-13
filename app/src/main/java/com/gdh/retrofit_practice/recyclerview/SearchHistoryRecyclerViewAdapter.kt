e10552f112eac18b900fda0901c3a4a8c6d03bb5
gumsu <chavi9@naver.com>
2021-02-14 00:23:31 +0900

    [#5] 검색어 저장 리싸이클러뷰 아답터, 뷰홀더 구현

--- app/src/main/java/com/gdh/retrofit_practice/recyclerview/SearchHistoryRecyclerViewAdapter.kt
+++ app/src/main/java/com/gdh/retrofit_practice/recyclerview/SearchHistoryRecyclerViewAdapter.kt
@@ -1,18 +1,31 @@
 package com.gdh.retrofit_practice.recyclerview
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.gdh.retrofit_practice.R
+import com.gdh.retrofit_practice.model.SearchData
 
 class SearchHistoryRecyclerViewAdapter: RecyclerView.Adapter<SearchItemViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchItemViewHolder {
+    private var searchHistoryList: ArrayList<SearchData> = ArrayList()
 
+    // 뷰홀더가 메모리에 올라갔을 때, 뷰홀더와 레이아웃을 연결시켜준다.
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchItemViewHolder {
+        val searchItemViewHolder = SearchItemViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.layout_search_item, parent, false))
+        return searchItemViewHolder
     }
 
     override fun onBindViewHolder(holder: SearchItemViewHolder, position: Int) {
-
+        val dataItem: SearchData = this.searchHistoryList[position]
+        holder.bindWithView(dataItem)
     }
 
     override fun getItemCount(): Int {
+        return searchHistoryList.size
+    }
 
+    // 외부에서 어답터에 데이터 배열을 넣어준다.
+    fun submitList(searchHistoryList: ArrayList<SearchData>){
+        this.searchHistoryList = searchHistoryList
     }
 }
--- app/src/main/java/com/gdh/retrofit_practice/recyclerview/SearchItemViewHolder.kt
+++ app/src/main/java/com/gdh/retrofit_practice/recyclerview/SearchItemViewHolder.kt
@@ -1,8 +1,44 @@
 package com.gdh.retrofit_practice.recyclerview
 
+import android.util.Log
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.gdh.retrofit_practice.model.SearchData
+import com.gdh.retrofit_practice.utils.Constants.TAG
+import kotlinx.android.synthetic.main.layout_search_item.view.*
 
-class SearchItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+class SearchItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView), View.OnClickListener {
 
+    // 뷰 가져오기
+    private val searchItemTextView = itemView.search_term_text
+    private val whenSearchedTextView = itemView.when_searched_text
+    private val deleteSearchBtn = itemView.delete_search_btn
+    // 몸통의 클릭 이벤트 처리를 위해
+    private val constraintSearchItem = itemView.constraint_search_item
+
+    init {
+        // onClick() 리스너 연결 처리
+        deleteSearchBtn.setOnClickListener(this)
+        constraintSearchItem.setOnClickListener(this)
+    }
+
+    // 데이터와 뷰를 묶는다.
+    fun bindWithView(searchItem: SearchData){
+        Log.d(TAG, "SearchItemViewHolder - bindWithView() called")
+
+        whenSearchedTextView.text = searchItem.timestamp
+        searchItemTextView.text = searchItem.term
+    }
+
+    override fun onClick(view: View?) {
+        Log.d(TAG, "SearchItemViewHolder - onClick() called ")
+        when(view){
+            deleteSearchBtn -> {
+                Log.d(TAG, "SearchItemViewHolder -  검색 삭제 버튼 클릭")
+            }
+            constraintSearchItem -> {
+                Log.d(TAG, "SearchItemViewHolder -  검색 아이템 클릭")
+            }
+        }
+    }
 }
