9ba58287007299c5ddd9b108865ca10738e9d5e7
gumsu <chavi9@naver.com>
2021-02-14 00:08:32 +0900

    [#5] 검색어 저장 UI 구현, 리싸이클러뷰 아답터, 뷰홀더 생성

---
+++ app/src/main/java/com/gdh/retrofit_practice/recyclerview/SearchHistoryRecyclerViewAdapter.kt
@@ -0,0 +1,18 @@
+package com.gdh.retrofit_practice.recyclerview
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class SearchHistoryRecyclerViewAdapter: RecyclerView.Adapter<SearchItemViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchItemViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: SearchItemViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+}
---
+++ app/src/main/java/com/gdh/retrofit_practice/recyclerview/SearchItemViewHolder.kt
@@ -0,0 +1,8 @@
+package com.gdh.retrofit_practice.recyclerview
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class SearchItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+}
